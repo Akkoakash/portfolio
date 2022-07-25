84000de5aeec0379e187f55d0ea85d8a4eef1391
Akash kumar <92292782+Akkoakash@users.noreply.github.com>
2022-07-25 16:03:50 +0530

    update resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,5 +6,5 @@ export const headerData = {
     desciption:"A software developer passionate about implementing and launching new projects. Looking to start my career as an entry-level software engineer with a reputed firm driven by technology. ",
     image: 'https://drive.google.com/uc?export=view&id=1cBUOVy9FEHuEQ9Z_TG3ePofbjfn4E6aM',
     resumePdf: resume,
-    resumeLink: "https://drive.google.com/file/d/1ggHUHsLatV4fhLMd4mgrOLMzcB3MCZeb/view?usp=sharing"
+    resumeLink: "https://drive.google.com/file/d/1w8rxPwvEUIYIyBcJe86wzeiU_L7h7gAa/view?usp=sharing"
 }
